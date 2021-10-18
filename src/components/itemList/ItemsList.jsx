d29542fd3bd9ab6e56b258a44d1eb663cddde1c0
John Glebov <egoji@mail.ru>
2021-10-18 15:49:52 +0300

    slice added

--- src/components/itemList/ItemsList.jsx
+++ src/components/itemList/ItemsList.jsx
@@ -2,15 +2,21 @@ import {Item} from '../item/Item';
 
 function ItemsList(props) {
   const { items = [], addToBasket = Function.prototype} = props;
+
+  const showMore = 15;
+
+
+
   if(!items.length) {
     return <h3>Ничегошеньки</h3>
   }
   return (
       <div className="item__list">
-          {items.map(item => (
+          {items.slice(0, showMore).map(item => (
               <Item key={item.mainId} {...item} addToBasket={addToBasket}/>
           ))}
       </div>
+      
   )
 }
 
